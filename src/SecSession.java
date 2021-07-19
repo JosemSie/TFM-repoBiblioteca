6230dcff32263d5b670a0319d95bf97a6a134aa9
JosemSie <36673200+JosemSie@users.noreply.github.com>
2021-07-19 02:26:55 +0200

    Código inicial
    Implementación del código inicial.

---
+++ src/SecSession.java
@@ -0,0 +1,23 @@
+
+public/* abstract*/ class SecSession {
+	private String lastURI;
+    private Object id;
+    public SecSession(Object paramId){
+        this.id = paramId;
+    }
+    public SecSession(Object paramId, String paramLastURI){
+        this.id = paramId;
+        this.lastURI = paramLastURI;
+    }
+    public String getLastURI(){
+        return this.lastURI;
+    }
+    public void setLastURI(String newURI){
+        this.lastURI = newURI;
+    }
+    public Object getId(){
+        return this.id;
+    }
+    //public abstract boolean isEqual(SecSession p1);
+
+}
---
+++ src/SecSessionController.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class SecSessionController{
+	
+	private HashMap<String,ArrayList<String>> map;
+	
+	public SecSessionController() {
+	        map = new HashMap<String,ArrayList<String>>();
+	}
+    public boolean isPossible(String newUri, SecSession actualSession){
+        boolean sol = false;
+        
+        for(String checkSession : map.get(actualSession.getLastURI())){
+            if(newUri.equals(checkSession)){
+                sol = true;
+                actualSession.setLastURI(newUri);
+            }
+        }
+        return sol;
+    }
+    public void createMap(String ruta) throws IOException{
+        String cadena;
+        FileReader f = new FileReader(ruta);
+        BufferedReader b = new BufferedReader(f);
+        while((cadena = b.readLine())!=null) {
+            String[] splitText = cadena.split(" ");
+            ArrayList<String> aux = new ArrayList<String>();
+            for(int i = 1; i<splitText.length;i++)aux.add(splitText[i]);
+            map.put(splitText[0], aux);
+        }
+        b.close();
+    }
+}
